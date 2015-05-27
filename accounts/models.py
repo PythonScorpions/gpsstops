7b550cb946545a55c2eda38df40b0f0689da9b95
scorpionsspython <scorpionsspython@gmail.com>
2015-05-28 02:02:43 +0530

    Auth token creation when new user is created

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 from django.core.validators import RegexValidator
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save,pre_save,pre_delete
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 class UserProfiles(models.Model):
