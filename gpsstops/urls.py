75cfe367e4835a81501d50cce454d66516134b77
scorpionsspython <scorpionsspython@gmail.com>
2015-05-28 00:53:17 +0530

    api base url added

--- gpsstops/urls.py
+++ gpsstops/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^', include('accounts.urls')),
+                       url(r'^api/', include('api.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
