cbf4d014f5b2d7fdf948fd976de1d4c2ce485da1
mjrulesamrat <mjrulesamrat@outlook.com>
2015-05-30 23:17:14 +0530

    maps app's url added

--- gpsstops/urls.py
+++ gpsstops/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^', include('accounts.urls')),
+                       url(r'^maps/', include('maps.urls')),
                        url(r'^api/', include('api.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
