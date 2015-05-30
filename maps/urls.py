da5444010f8eb1db0c6e91cedab2979e9eb23b24
mjrulesamrat <mjrulesamrat@outlook.com>
2015-05-31 01:25:31 +0530

    Edit route url added

--- maps/urls.py
+++ maps/urls.py
@@ -3,5 +3,6 @@ from maps.views import *
 
 urlpatterns = patterns("maps.views",
                        url(r'create_route$', Create_Route.as_view(), name='create_route'),
+                       url(r'edit_route/(?P<id>.*)/$', Edit_Route.as_view(), name='edit_route'),
                     #    url(r'signup$', register, name='signup'),
                        )
