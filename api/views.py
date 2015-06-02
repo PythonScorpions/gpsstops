999aa7c1d6fe8cab7b1232865286cc2050067ae2
scorpionsspython <scorpionsspython@gmail.com>
2015-06-02 18:02:17 +0530

    token generation on api

--- api/views.py
+++ api/views.py
@@ -15,6 +15,8 @@ from rest_framework import renderers
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from django.template import loader
 from django.template import RequestContext, Context
+import string
+import random
 
 
 for user in User.objects.all():
@@ -29,26 +31,28 @@ class CreateUser(APIView):
         if User.objects.filter(email=request.data['email']):
             return Response({'code': 1, 'status': 200, 'message': 'Email Already Exist'})
         else:
-            # try:
-            user_data = User(email=request.data['email'], username=request.data['email'],)
-            user_data.first_name = request.data['name']
-            user_data.password = make_password(request.data['password'])
-            user_data.is_active = False
-            user_data.save()
-            user_profile_data = UserProfiles(user=user_data, address=request.data['address'],
-                                             city=request.data['city'], state=request.data['state'],
-                                             zip_code=request.data['zip_code'], country=request.data['country'],
-                                             phone_number=request.data['phone_number'],
-                                             occupation=request.data['occupation'],
-                                             company_name=request.data['company_name'])
-            user_profile_data.save()
-            message = 'Please verify your email by clicking on this link ' + 'http://gpsstops.pythonanywhere.com/verification/'+user_profile_data.token
+            try:
+                user_data = User(email=request.data['email'], username=request.data['email'],)
+                user_data.first_name = request.data['name']
+                user_data.password = make_password(request.data['password'])
+                user_data.is_active = False
+                user_data.save()
+                user_profile_data = UserProfiles(user=user_data, address=request.data['address'],
+                                                 city=request.data['city'], state=request.data['state'],
+                                                 zip_code=request.data['zip_code'], country=request.data['country'],
+                                                 phone_number=request.data['phone_number'],
+                                                 occupation=request.data['occupation'],
+                                                 company_name=request.data['company_name'])
+                alphabet = [c for c in string.letters + string.digits if ord(c) < 128]
+                user_profile_data.token = ''.join([random.choice(alphabet) for x in xrange(30)])
+                user_profile_data.save()
+                message = 'Please verify your email by clicking on this link ' + 'http://gpsstops.pythonanywhere.com/verification/'+user_profile_data.token
 
-            send_mail('Verification Link', message, 'scorpionspython@gmail.com', [str(user_data.email)],
-                      fail_silently=False)
-            return Response({'code': 0, 'status': 200, 'Data': 'Null', 'message': 'User has been created'})
-            # except:
-            #     return Response({'code': 1, 'status': 200, 'message': 'All fields are mandatory'})
+                send_mail('Verification Link', message, 'scorpionspython@gmail.com', [str(user_data.email)],
+                          fail_silently=False)
+                return Response({'code': 0, 'status': 200, 'Data': 'Null', 'message': 'User has been created'})
+            except:
+                return Response({'code': 1, 'status': 200, 'message': 'All fields are mandatory'})
 
 
 class UpdateUser(APIView):
