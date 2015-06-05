0233d6d19bd489ac0e453d6aebbda4c0993bff63
PythonScorpions <scorpionspython@gmail.com>
2015-06-05 14:31:01 +0530

    views changed for edit route

--- maps/views.py
+++ maps/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView, UpdateView, View
 from django.shortcuts import render_to_response, redirect, render
 from maps.models import *
 import datetime
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -18,9 +19,9 @@ class Create_Route(View):
     def post(self, request):
         print "came in POST"
         trip_title = request.POST['trip_title']
-        trip_datetime = datetime.datetime.strptime(str(request.POST['trip_datetime']), "%m/%d/%Y %I:%M %p")
+        trip_datetime = datetime.datetime.strptime(str(request.POST['trip_datetime']), "%Y/%m/%d %H:%M")
         print trip_title
-        print type(trip_datetime),"-----------------",trip_datetime
+        print type(trip_datetime), "-----------------", trip_datetime
         total_time = request.POST['total_hours']
         if '.' in request.POST['total_distance']:
             total_distance = float(request.POST['total_distance'][:-3])
@@ -38,18 +39,15 @@ class Create_Route(View):
         end_note_location = request.POST['end_note_location']
         latitude_last = request.POST['latitude_last']
         longitude_last = request.POST['longitude_last']
-        route_obj = Route(user=request.user, trip_title=trip_title, trip_datetime=trip_datetime, total_distance=total_distance,
-        total_time=total_time)
+        route_obj = Route(user=request.user, trip_title=trip_title, trip_datetime=trip_datetime,
+                          total_distance=total_distance, total_time=total_time)
         route_obj.save()
 
         total_waypoint = int(request.POST['total_waypoint'])
 
-        Location(route=route_obj,location_address = start_search_address, location_near_address= start_near_address,
-        location_lat=latitude_first, location_long=longitude_first,location_note=start_note_location, location_number=11).save()
-
-        Location(route=route_obj,location_address = end_search_address, location_near_address= end_near_address,
-        location_lat=latitude_last, location_long=longitude_last,location_note=end_note_location, location_number=22).save()
-
+        Location(route=route_obj, location_address=start_search_address, location_near_address=start_near_address,
+                 location_lat=latitude_first, location_long=longitude_first, location_note=start_note_location,
+                 location_number=11).save()
 
         for i in xrange(1, total_waypoint+1):
             search_add_n = "search_address"+str(i)
@@ -63,14 +61,17 @@ class Create_Route(View):
             lat_data = request.POST[latitude_n]
             lng_data = request.POST[longitude_n]
 
-            Location(route=route_obj,location_address = search_address, location_near_address= near_address,
-            location_lat=lat_data, location_long=lng_data, location_note=note, location_number=i).save()
+            Location(route=route_obj, location_address=search_address, location_near_address=near_address,
+                     location_lat=lat_data, location_long=lng_data, location_note=note, location_number=i).save()
+
+        Location(route=route_obj, location_address=end_search_address, location_near_address=end_near_address,
+                 location_lat=latitude_last, location_long=longitude_last, location_note=end_note_location,
+                 location_number=22).save()
 
         # trip_title = request.POST['trip_title']
         active = "maps"
-        flag="maps"
-        return render(request, self.template2, locals())
-
+        flag = "maps"
+        return HttpResponseRedirect('/maps/routes')
 
 
 class Edit_Route(View):
@@ -80,7 +81,7 @@ class Edit_Route(View):
     def get(self, request, **kwargs):
         print "came in to get"
         id = int(self.kwargs['id'])
-        route_obj = Route.objects.get(id = id)
+        route_obj = Route.objects.get(id=id)
         print route_obj.trip_title
         start_end =[11, 22]
         location_obj = Location.objects.filter(route = route_obj).order_by('id')
@@ -92,15 +93,17 @@ class Edit_Route(View):
 
         active = "maps"
         flag="maps"
+        today = datetime.date.today()
+        routes = Route.objects.filter(user=request.user, trip_datetime__startswith=today).exclude(id=id)
+
         return render(request, self.template1, locals())
 
-    def post(self, request):
+    def post(self, request, id):
         print "came in POST"
         trip_title = request.POST['trip_title']
-        trip_datetime = datetime.datetime.strptime(str(request.POST['trip_datetime']), "%m/%d/%Y %I:%M %p")
+        trip_datetime = datetime.datetime.strptime(str(request.POST['trip_datetime']), "%Y/%m/%d %H:%M")
         print trip_title
-        print type(trip_datetime),"-----------------",trip_datetime
-        raw_input("wait here")
+        print type(trip_datetime), "-----------------", trip_datetime
         total_time = request.POST['total_hours']
         if '.' in request.POST['total_distance']:
             total_distance = float(request.POST['total_distance'][:-3])
@@ -118,18 +121,21 @@ class Edit_Route(View):
         end_note_location = request.POST['end_note_location']
         latitude_last = request.POST['latitude_last']
         longitude_last = request.POST['longitude_last']
-        route_obj = Route(user=request.user, trip_title=trip_title, trip_datetime=trip_datetime, total_distance=total_distance,
-        total_time=total_time)
+        route_obj = Route.objects.get(id=int(id))
+        route_obj.trip_title = trip_title
+        route_obj.trip_datetime = trip_datetime
+        route_obj.total_distance = total_distance
+        route_obj.total_time = total_time
+
         route_obj.save()
+
+        Location.objects.filter(route=route_obj).delete()
 
         total_waypoint = int(request.POST['total_waypoint'])
 
-        Location(route=route_obj,location_address = start_search_address, location_near_address= start_near_address,
-        location_lat=latitude_first, location_long=longitude_first,location_note=start_note_location, location_number=11).save()
-
-        Location(route=route_obj,location_address = end_search_address, location_near_address= end_near_address,
-        location_lat=latitude_last, location_long=longitude_last,location_note=end_note_location, location_number=22).save()
-
+        Location(route=route_obj, location_address=start_search_address, location_near_address=start_near_address,
+                 location_lat=latitude_first, location_long=longitude_first, location_note=start_note_location,
+                 location_number=11).save()
 
         for i in xrange(1, total_waypoint+1):
             search_add_n = "search_address"+str(i)
@@ -143,21 +149,26 @@ class Edit_Route(View):
             lat_data = request.POST[latitude_n]
             lng_data = request.POST[longitude_n]
 
-            Location(route=route_obj,location_address = search_address, location_near_address= near_address,
-            location_lat=lat_data, location_long=lng_data, location_note=note, location_number=i).save()
+            Location(route=route_obj, location_address=search_address, location_near_address=near_address,
+                     location_lat=lat_data, location_long=lng_data, location_note=note, location_number=i).save()
+
+        Location(route=route_obj, location_address=end_search_address, location_near_address=end_near_address,
+                 location_lat=latitude_last, location_long=longitude_last, location_note=end_note_location,
+                 location_number=22).save()
 
         # trip_title = request.POST['trip_title']
         active = "maps"
-        flag="maps"
-        return render(request, self.template2, locals())
+        flag = "maps"
+        return HttpResponseRedirect('/maps/routes')
 
 
 class Routes(View):
     template1 = "routes.html"
 
     def get(self, request):
-        routes = Route.objects.filter(user=request.user)
-
+        today = datetime.date.today()
+        routes = Route.objects.filter(user=request.user, trip_datetime__startswith=today)
+        print routes
         active = "maps"
         flag = "maps"
         return render(request, self.template1, locals())
