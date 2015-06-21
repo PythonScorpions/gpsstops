ab6e29244fec2233cf5e191f8621c4ed5108bcd2
scorpionsspython <scorpionsspython@gmail.com>
2015-06-22 00:24:20 +0530

    admin url changed

--- gpsstops/urls.py
+++ gpsstops/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^', include('accounts.urls')),
                        url(r'^maps/', include('maps.urls')),
                        url(r'^api/', include('api.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include('siteadmin.urls')),
                        )
