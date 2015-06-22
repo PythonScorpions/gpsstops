21d66da142e0e667c73d0eee2b4d2065353663fe
scorpionsspython <scorpionsspython@gmail.com>
2015-06-23 02:12:57 +0530

    ff

--- gpsstops/urls.py
+++ gpsstops/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
+from accounts.views import *
 
 urlpatterns = patterns('',
                        url(r'^', include('accounts.urls')),
                        url(r'^maps/', include('maps.urls')),
                        url(r'^api/', include('api.urls')),
                        url(r'^admin/', include('siteadmin.urls')),
+                       url(r'download/', Download.as_view(), name='download'),
+                       url(r'support/', Support.as_view(), name='support'),
+                       url(r'contact_us/', Contact.as_view(), name='contact'),
+                       url(r'about_us/', About.as_view(), name='about'),
                        )
