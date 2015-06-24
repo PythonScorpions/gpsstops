2c28758bf2941ba14a6553bafb0eb598d3eaeb0f
scorpionsspython <scorpionsspython@gmail.com>
2015-06-25 02:32:05 +0530

    asasqwq

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken import views
 urlpatterns = patterns("api.views",
                        url(r'create_user', CreateUser.as_view(), name='create-user'),
                        url(r'update_user/(?P<pk>[a-zA-Z0-9]+)', UpdateUser.as_view(), name='update-user'),
-                       url(r'^api-token-auth/', LoginUser.as_view(), name='token-check'),
+                       url(r'^api-token-auth/', ObtainAuthToken.as_view(), name='token-check'),
                        url(r'current_user_details/(?P<pk>[a-zA-Z0-9]+)', CurrentUser.as_view(), name='current-user'),
                        url(r'country_list', CountryList.as_view(), name='country-list'),
                        url(r'forgot_password', ForgotPassword.as_view(), name='country-list'),
