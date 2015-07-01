fd2d0cc4a08b008c4f2272322b01f64d9fe8a28c
scorpionsspython <scorpionsspython@gmail.com>
2015-07-01 21:36:23 +0530

    Email credentials password strong

--- gpsstops/settings.py
+++ gpsstops/settings.py
@@ -122,7 +122,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gpsstopsapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'gps@1234'
+EMAIL_HOST_PASSWORD = 'Gpsstops99@@'
 EMAIL_PORT = 587
 
 
