82c11c79bf2bc9bd6b8b7a96221e1e692917810c
scorpionsspython <scorpionsspython@gmail.com>
2015-07-01 22:28:31 +0530

    password problem 2

--- gpsstops/settings.py
+++ gpsstops/settings.py
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'gpsstopsapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'Gpsstops9977((@@'
+EMAIL_HOST_USER = 'gpsstop99@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gpsstops99@@'
 EMAIL_PORT = 587
 
 
