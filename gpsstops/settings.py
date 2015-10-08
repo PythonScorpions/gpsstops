4e003069ef42a47209d8dbafd32104c14ac00fd8
Prathamesh Paiyyar <mailprathamesh27@gmail.com>
2015-10-08 13:20:46 +0530

    ic

--- gpsstops/settings.py
+++ gpsstops/settings.py
@@ -189,11 +189,11 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = (
-#     STATIC_ROOT,
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
